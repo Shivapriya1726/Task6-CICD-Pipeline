7c069cf976032d526e6e3e9304a54e42377bfffa
Shivapriya <90460346+Shivapriya1726@users.noreply.github.com>
2024-04-01 17:20:46 +0530

    Update Test.java

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,32 +1,23 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-import static org.junit.Assert.assertTrue;
-
-public class Test {
-    public static void main(String[] args) {
-        // Set the path to the ChromeDriver executable
-        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-
-        // Initialize ChromeDriver instance
-        WebDriver driver = new ChromeDriver();
-
-        // Open a webpage
-        driver.get("https://www.google.com");
-
-        WebElement searchBox = driver.findElement(By.name("q"));
-        searchBox.sendKeys("Selenium");
-        searchBox.submit();
-
-        // Verify search results
-        assertTrue(driver.getTitle().contains("Selenium"));
-
-        // Close the browser
-        driver.quit();
-        System.out.println("Test Passed");
+public class CalculatorTest {
+    
+    @Test
+    public void testAddition() {
+        int a = 5;
+        int b = 7;
+        Calculator calculator = new Calculator();
+        int result = calculator.add(a, b);
+        Assert.assertEquals(result, 12, "Addition result is incorrect");
+    }
+    
+    @Test
+    public void testSubtraction() {
+        int a = 10;
+        int b = 3;
+        Calculator calculator = new Calculator();
+        int result = calculator.subtract(a, b);
+        Assert.assertEquals(result, 7, "Subtraction result is incorrect");
     }
 }
-
-
